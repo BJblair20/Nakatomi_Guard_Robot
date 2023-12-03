11cec2d6e098952e0db6a822c715d0b560a7d39a
BJBlair20 <Brand.j.Blair@gmail.com>
2023-12-03 15:51:58 -0500

    y

--- RaspberryPi/Facial Recognition/faceRec.py
+++ RaspberryPi/Facial Recognition/faceRec.py
@@ -97,10 +97,10 @@ class FaceRecognition:
                 cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0,0,255), -1)
                 #cv2.putText(frame, name, (left + 6, bottom -6), cv2.FONT_HERSHEY_DUPLEX, 0.8 (255, 255, 255), 1)
 
-            cv2.imshow('Face Recognition', frame)
+            #cv2.imshow('Face Recognition', frame)
 
-            if cv2.waitKey(1)==ord('q'):
-                break
+            #if cv2.waitKey(1)==ord('q'):
+            #    break
 
         video_capture.release()
         cv2.destroyAllWindows()
