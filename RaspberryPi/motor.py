ad40582384b3814b95cbe8da32af20a29041de0a
BJBlair20 <Brand.j.Blair@gmail.com>
2023-12-03 18:14:45 -0500

    Why

--- RaspberryPi/motor.py
+++ RaspberryPi/motor.py
@@ -3,8 +3,8 @@ from adafruit_motorkit import MotorKit
 import random
 kit = MotorKit()
 def runMotor():
-    kit.motor1.throttle = 1
-    kit.motor2.throttle = -1
+    kit.motor1.throttle = 0.6
+    kit.motor2.throttle = -0.6
 
 def stopMotor():
     kit.motor1.throttle = 0
