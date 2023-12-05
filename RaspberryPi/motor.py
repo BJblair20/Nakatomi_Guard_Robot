4997927a37d3df1c4a38d6c713e2585fafb5be09
BJBlair20 <Brand.j.Blair@gmail.com>
2023-12-05 13:39:17 -0500

    y

--- RaspberryPi/motor.py
+++ RaspberryPi/motor.py
@@ -15,17 +15,17 @@ def stopMotor():
 def turn():
     kit.motor1.throttle = 0
     kit.motor2.throttle = 0
-    time.sleep(1)
+    time.sleep(1.5)
     flip = random.randint(0, 1)
 
     if flip == 0:
         kit.motor1.throttle = 0.8
         kit.motor2.throttle = 0.8
-        time.sleep(0.0025)
+        time.sleep(0.1)
     else:
         kit.motor1.throttle = -0.8
         kit.motor2.throttle = -0.8
-        time.sleep(0.0025)
+        time.sleep(0.1)
 
 if __name__ == "__main__":
     kit.motor1.throttle = 0
