9f9b7e1d3fe891ce7a865019919f14c72836f082
BJBlair20 <Brand.j.Blair@gmail.com>
2023-12-06 17:23:19 -0500

    YUR

--- RaspberryPi/preview.py
+++ RaspberryPi/preview.py
@@ -5,6 +5,14 @@ audio_file = os.path.dirname(__file__) + '/Sound/alarmSound.wav'
 
 import subprocess
 
+def get_current_user():
+    try:
+        result = subprocess.run(['whoami'], capture_output=True, text=True, check=True)
+        return result.stdout.strip()
+    except subprocess.CalledProcessError as e:
+        print(f'Error getting current user: {e}')
+        return None
+    
 def change_user(new_user):
     try:
         subprocess.run(['sudo', '-u', new_user, 'ls'])
@@ -15,4 +23,5 @@ def change_user(new_user):
 if __name__ == "__main__":
     new_user = "pi"
     change_user(new_user)
+    print("OUTPUT: " + get_current_user())
     subprocess.run(['whoami'])
