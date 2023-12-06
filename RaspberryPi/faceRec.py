5673dc1702095599387476af8f992ba606757cd1
BJBlair20 <Brand.j.Blair@gmail.com>
2023-12-05 21:01:25 -0500

    YUR

--- RaspberryPi/faceRec.py
+++ RaspberryPi/faceRec.py
@@ -104,7 +104,7 @@ class FaceRecognition:
                 
                 cv2.rectangle(frame, (left, top), (right, bottom), (0,0,255), 2)
                 cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0,0,255), -1)
-                #cv2.putText(frame, name, (left + 6, bottom -6), cv2.FONT_HERSHEY_DUPLEX, 0.8 (255, 255, 255), 1)
+                cv2.putText(frame, name, (left + 6, bottom -6), cv2.FONT_HERSHEY_DUPLEX, 0.8 (255, 255, 255), 1)
 
             cv2.imshow('Face Recognition', rgb_small_frame)
 
