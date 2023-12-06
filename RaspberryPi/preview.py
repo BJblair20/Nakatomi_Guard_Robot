0828aaf6cc16806fde7b74756167a72bfcb2c00d
BJBlair20 <Brand.j.Blair@gmail.com>
2023-12-06 17:15:27 -0500

    YUR

--- RaspberryPi/preview.py
+++ RaspberryPi/preview.py
@@ -4,7 +4,7 @@ import subprocess
 audio_file = os.path.dirname(__file__) + '/Sound/alarmSound.wav'
 #aplay sound.wav
 
-subprocess.run(["su","pi"])
+subprocess.run(["sudo","-u","pi"])
 subprocess.run(["whoami"])
 print("Here?")
 subprocess.run(["aplay", audio_file]) 
