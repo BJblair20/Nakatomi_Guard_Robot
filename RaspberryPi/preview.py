4a6f581d8c67a89a74cf53114c5abbe1d78cca41
BJBlair20 <Brand.j.Blair@gmail.com>
2023-12-06 17:19:17 -0500

    YUR

--- RaspberryPi/preview.py
+++ RaspberryPi/preview.py
@@ -2,9 +2,16 @@ import pygame
 import os
 import subprocess
 audio_file = os.path.dirname(__file__) + '/Sound/alarmSound.wav'
-#aplay sound.wav
 
-subprocess.run(["sudo","-u","pi"])
-subprocess.run(["whoami"])
-print("Here?")
-subprocess.run(["aplay", audio_file]) 
+import subprocess
+
+def change_user(new_user):
+    try:
+        subprocess.run(['sudo', '-u', new_user, 'ls'])
+        print(f'Successfully switched to user: {new_user}')
+    except subprocess.CalledProcessError as e:
+        print(f'Error switching user: {e}')
+
+if __name__ == "__main__":
+    new_user = "pi"
+    change_user(new_user)
