5b2ee0c38752277ea1cc77fa7126f0d4c038786f
BJBlair20 <Brand.j.Blair@gmail.com>
2023-12-07 00:35:07 -0500

    YUR

--- RaspberryPi/main.py
+++ RaspberryPi/main.py
@@ -7,7 +7,7 @@ import lights
 import distanceReader
 import motor
 import pandas as pd
-import pygame
+#import pygame
 import neopixel
 import board
 import subprocess
@@ -48,8 +48,8 @@ def soundCue():
     print("ALERT SOUND")
     audio_file = os.path.dirname(__file__) + '/Sound/calm.wav'
     while not lightControl.is_set():
-        subprocess.run(["aplay", audio_file]) 
-        time.sleep(3)
+        subprocess.run(["aplay", audio_file],stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) 
+        #time.sleep(3)
     """
     pygame.mixer.init()
     audio_file = os.path.dirname(__file__) + '/Sound/calm.wav'
@@ -68,8 +68,8 @@ def alarmCue():
     #import subprocess
     
     while not lightControl.is_set():
-        subprocess.run(["aplay", audio_file]) 
-        time.sleep(3)
+        subprocess.run(["aplay", audio_file],stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) 
+        #time.sleep(1)
     """
     pygame.mixer.init()
     audio_file = os.path.dirname(__file__) + '/Sound/alarmSound.wav'
@@ -83,6 +83,11 @@ def alarmCue():
             
 
 def greeting():
+    print("GREETING SOUND")
+    audio_file = os.path.dirname(__file__) + '/Sound/welcome.wav'
+    subprocess.run(["aplay", audio_file],stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) 
+
+    """
     print("WELCOME")
     pygame.mixer.init()
     audio_file = os.path.dirname(__file__) + '/Sound/welcome.wav'
@@ -90,7 +95,7 @@ def greeting():
     pygame.mixer.music.play()
     while pygame.mixer.music.get_busy() == True:
         continue
-
+    """
 #################################################################################################################
 #################........................MOTOR..........................#########################################
 #################################################################################################################
@@ -152,11 +157,13 @@ def actionLists(color):
 
     if data["halt"][int(color)]==1:
         print("NAKATOMI BOT:")
-        input("CLEAR TO CONTINUE? ")
+        userIn = input("CLEAR TO CONTINUE? ")
 
     print("GOT line 142 ")
-    time.sleep(8)
+    #time.sleep(4)
     lightControl.set()
+    time.sleep(3)
+    lightControl.clear()
     
     
 
@@ -174,19 +181,23 @@ motorControl = threading.Event()
 lightControl = threading.Event()
 #runMotor()
 
-t1 = threading.Thread(target=runMotor, name='t1')
-t2 = threading.Thread(target=camera, name='t2')
-#t3 = threading.Thread(target=alarmCue, name='t3')
-#t4 = threading.Thread(target=lightsAlarm, name='t4')
+while True:
+    t1 = threading.Thread(target=runMotor, name='t1')
+    t2 = threading.Thread(target=camera, name='t2')
+    #t3 = threading.Thread(target=alarmCue, name='t3')
+    #t4 = threading.Thread(target=lightsAlarm, name='t4')
 
-#global motorControl
-#motorControl = True
+    #global motorControl
+    #motorControl = True
 
-#alarmCue()
-t2.start()
-time.sleep(10)
-t1.start()
-time.sleep(10)
+    #alarmCue()
+    t2.start()
+    time.sleep(10)
+    t1.start()
+    time.sleep(10)
+
+    print("ENDED BOTH MAIN THREADS")
+    motorControl.clear()
 #t3.start()
 #t4.start()
  
