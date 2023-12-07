e46e8606ce46e3f36ee11b38b452f142bba20fc8
BJBlair20 <Brand.j.Blair@gmail.com>
2023-12-07 01:11:24 -0500

    YUR

--- RaspberryPi/motor.py
+++ RaspberryPi/motor.py
@@ -16,18 +16,22 @@ def turn():
     kit.motor1.throttle = 0
     kit.motor2.throttle = 0
     time.sleep(1.5)
+    kit.motor1.throttle = -0.5
+    kit.motor2.throttle = 0.5
+    time.sleep(3)
+
     flip = random.randint(0, 1)
 
     if flip == 0:
         kit.motor1.throttle = 0.8
         kit.motor2.throttle = 0.8
-        time.sleep(0.75)
+        time.sleep(1.25)
         kit.motor1.throttle = 0
         kit.motor2.throttle = 0
     else:
         kit.motor1.throttle = -0.8
         kit.motor2.throttle = -0.8
-        time.sleep(0.75)
+        time.sleep(1.25)
         kit.motor1.throttle = 0
         kit.motor2.throttle = 0
 
