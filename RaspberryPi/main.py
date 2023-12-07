2036fbc187c8383c23fd1d196711c99f21afce92
BJBlair20 <Brand.j.Blair@gmail.com>
2023-12-06 23:43:47 -0500

    YUR

--- RaspberryPi/main.py
+++ RaspberryPi/main.py
@@ -64,8 +64,8 @@ def soundCue():
     pygame.mixer.music.load(audio_file)
     while not lightControl.is_set():
         pygame.mixer.music.play()
-        while pygame.mixer.music.get_busy() == True:
-            continue
+        #while pygame.mixer.music.get_busy() == True:
+        #    continue
 
 def alarmCue():
     #global motorControl
