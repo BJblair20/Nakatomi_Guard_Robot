069c152599b2a694fc55e1d66b6b580bc86936ea
BJBlair20 <Brand.j.Blair@gmail.com>
2023-12-06 23:27:53 -0500

    YUR

--- RaspberryPi/lightsFiles/alertLights.py
+++ RaspberryPi/lightsFiles/alertLights.py
@@ -3,11 +3,10 @@ import board
 import time
 
 if __name__ == '__main__':
-
     pixels = neopixel.NeoPixel(board.D18, 32)
     pixels.brightness = .5
     for x in range (0,32):
-        pixels = (255,211,0)
-    time.sleep(0.5)
+        pixels[x] = (255,211,0)
+    time.sleep(1)
     for x in range (0,32):
-        pixels = (0,0,0)
+        pixels[x] = (0,0,0)
